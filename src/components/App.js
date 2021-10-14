fd5c126ca6b7fbb6a6562a049005d7fbf6768a5a
EnzoFB <enzo.ferreirabastos@gmail.com>
2021-10-14 15:05:39 +0200

    last commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Banner from './Banner'
 import Cart from './Cart';
 import Footer from './Footer';
 import ShoppingList from './ShoppingList'
-
-/*
-const App = () => <div>
-    <Banner/>
-    <Cart/>
-    <ShoppingList/>
-    <Footer />
-</div>
-*/
+import '../styles/Layout.css'
 
 function App() {
+    // On récupere le panier dans le local storage
+    const savedCart = localStorage.getItem('cart')
 
-    const [cart, updateCart] = useState([])
+    // Si savedCart existe, on le parse pour l'utiliser comme valeur par défaut, sinon, on utilise par défaut un tableau vide 
+    const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+
+    // On transforme une valeur (le panier) en objet utilisable
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    })
 
     return <div>
         <Banner/>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,33 +1,39 @@
 import '../styles/Cart.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Cart = ({cart, updateCart}) => { 
 
     //const [cart, updateCart] = useState(0)              // Décomposition (Tableau) de useState(Valeur par défault)$
     const [isOpen, setIsOpen] = useState(true)
-    /*
-    const totalPrice = (...prices) => {
-        let sum = 0;
-        prices.forEach(price => {
-            sum = sum + price
-        });
-        return sum;
-    }
-    */
+    
+    const totalPrice = cart.reduce((acc, plant) => acc + plant.amount * plant.price, 0)
+    
+    useEffect(() => {
+        document.title = `LMJ: ${totalPrice}€ d'achats`
+    }, [totalPrice])
 
     function displayCart({name, price, amount, index}) {
         return <li key={`${name}-${index}`}>{name} {price}€ x {amount}</li>
     }
-    
+
     return isOpen ? (
         <div className="cart">
-            <button className="cart-toggle-button" onClick={() => setIsOpen(false)}>Fermer</button>
-            <h2>Votre Panier:</h2>
-            <ul>
-                {cart.map(displayCart)}
-            </ul>
-            <p>Total du panier: {cart.reduce((acc, plant) => acc + plant.amount * plant.price, 0)} €</p>
-            <button onClick={() => updateCart([])}>Vider le panier</button>
+            <button className="cart-toggle-button" 
+                onClick={() => setIsOpen(false)}>
+                Fermer
+            </button>
+            { cart.length > 0 ? (
+                <div>
+                    <h2>Votre Panier:</h2>
+                    <ul>
+                        {cart.map(displayCart)}
+                    </ul>
+                    <p>Total du panier: {totalPrice} €</p>
+                    <button onClick={() => updateCart([])}>Vider le panier</button>
+                </div>
+            ) : (
+                <div><h2>Votre panier est vide</h2></div>
+            )}
         </div>
         ) : (
             <div className="cart-closed">
